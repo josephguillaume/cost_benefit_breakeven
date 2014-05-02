d8c76e4491256bf2df78b14d2edc47ee2f7255cf
Joseph Guillaume <josephguillaume@gmail.com>
2014-05-02 11:22:16 +1000

    createFun uses NPV from GlobalEnv

--- R/createFun.R
+++ R/createFun.R
@@ -1,6 +1,6 @@
 createFun <-
-function(scen,baseline,ranges,fixed.vals=NULL){
-  pars <- getDefaultPars(NPV)
+function(scen,baseline,ranges,fixed.vals=NULL,MODEL=NPV){
+  pars <- getDefaultPars(MODEL)
   for(v in ranges$Variable) {
     if(is.null(eval(parse(text=sprintf("pars$%s",v))))) stop(sprintf("Parameter %s not recognised",v))
   }
@@ -8,13 +8,21 @@ function(scen,baseline,ranges,fixed.vals=NULL){
   fixed.vals2 <- paste(sapply(1:length(fixed.vals),function(i) sprintf("pars$%s <- %s",names(fixed.vals)[i],fixed.vals[[i]])),collapse="\n    ")
   if(is.null(fixed.vals)) fixed.vals2 <- ""
   settings <- paste(sapply(1:nrow(ranges),function(i) sprintf("pars$%s <- x[%d]",ranges[i,1],i)),collapse="\n    ")
-  eval(parse(text=sprintf("
+  f <-
+      eval(parse(text=sprintf("
   function(x){
     x<-as.numeric(x)
     pars <- getDefaultPars(NPV)
     %s
     pars$scen='%s'
     %s
+    if(any(!names(pars) %%in%% names(formals(NPV))))
+     warning(sprintf('Variables are given in ranges etc but not used: %%s',
+      paste(names(pars)[!names(pars) %%in%% names(formals(NPV))],
+       collapse=', '
+      )
+     ))
+    pars=pars[names(formals(NPV))]
     s=do.call(NPV,pars)
     pars$scen='%s'
     b=do.call(NPV,pars)
@@ -22,4 +30,6 @@ function(scen,baseline,ranges,fixed.vals=NULL){
     stopifnot(!is.na(diff))
     diff
   }",fixed.vals2,scen,settings,baseline)))
+  environment(f)=.GlobalEnv
+  f
 }
